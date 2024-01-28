d234011675851f62f9151436dd65b221c961c98b
Isaias D Garcia <89608187+isaias-dgr@users.noreply.github.com>
2024-01-27 20:44:45 -0600

    add auto format

--- 002_DynamicStructs/main.py
+++ 002_DynamicStructs/main.py
@@ -2,9 +2,10 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-    
+
     def __str__(self):
         return str(self.data)
+
 
 class LinkedList:
     def __init__(self):
@@ -23,23 +24,23 @@ class LinkedList:
         current = self.head
         next = None if not self.head else self.head.next
         prev = None
-        count = 0 
-        if  position < 0:
+        count = 0
+        if position < 0:
             raise IndexError()
 
         while current:
-           if count == position:
-            if not prev:
-                self.head = new_node
-                new_node.next = current
-            else:
-                prev.next = new_node
-                new_node.next = current
-            return
-           prev = current
-           current = next
-           next = None if not next else next.next
-           count += 1
+            if count == position:
+                if not prev:
+                    self.head = new_node
+                    new_node.next = current
+                else:
+                    prev.next = new_node
+                    new_node.next = current
+                return
+            prev = current
+            current = next
+            next = None if not next else next.next
+            count += 1
 
         if count < position:
             raise IndexError()
@@ -48,30 +49,30 @@ class LinkedList:
         current = self.head
         next = None if not self.head else self.head.next
         prev = None
-        count = 0 
-        if  position < 0:
+        count = 0
+        if position < 0:
             raise IndexError()
 
         while current:
-           if count == position:
-            if not prev:
-                self.head = next
-            else:
-                prev.next = next
-            return
-           prev = current
-           current = next
-           next = None if not next else next.next
-           count += 1
+            if count == position:
+                if not prev:
+                    self.head = next
+                else:
+                    prev.next = next
+                return
+            prev = current
+            current = next
+            next = None if not next else next.next
+            count += 1
 
         if count < position:
             raise IndexError()
 
-    def push (self, node):
+    def push(self, node):
         node.next = self.head
-        self.head = node 
+        self.head = node
 
-    def pop (self):
+    def pop(self):
         node = self.head
         self.head = self.head.next if self.head else None
         return node
@@ -85,11 +86,11 @@ class LinkedList:
     def __str__(self):
         output = "["
         output += "" + str(self.head) + ", " if self.head else ""
-        node =  None if not self.head else self.head.next
+        node = None if not self.head else self.head.next
         while node:
             output += str(node) + (", " if node.next else "")
             node = node.next
-        return output+ "]"
+        return output + "]"
 
     def __iter__(self):
         node = self.head
